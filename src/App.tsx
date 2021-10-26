e55f0ec15c28aa7a66e2b8718b1a1eaf31a02fc1
ddfilipov <denisfilipov12+3@gmail.com>
2021-10-26 09:44:54 +0200

    added a POST method

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import axios from "axios";
 import React from "react";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function App() {
   const [post, setPost] = React.useState<any>(null);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
+    axios.get(`${baseURL}/1`).then((response) => {
       setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return <p>There are no more posts</p>
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
